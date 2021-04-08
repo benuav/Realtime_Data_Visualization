5ddcd3e55205264bca30c0027e5252b5152d7d38
Ben Li <54297796+pleycothh@users.noreply.github.com>
2021-04-09 00:35:38 +1000

    set up for start

---
+++ Choropleth_Map_D3/script.js
@@ -0,0 +1,13 @@
+let countyURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
+let educationURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
+
+
+
+console.log(d3)
+console.log(topojson)
+
+let canvas = d3.select('#canvas')
+
+let drawMap = () => {
+
+}
