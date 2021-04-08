5c33db8197e838057e6366f45a0ecac55d808a47
Ben Li <54297796+pleycothh@users.noreply.github.com>
2021-04-09 01:47:03 +1000

    draw the basic map shap from .json

--- Choropleth_Map_D3/script.js
+++ Choropleth_Map_D3/script.js
@@ -9,5 +9,39 @@ console.log(topojson)
 let canvas = d3.select('#canvas')
 
 let drawMap = () => {
+ 
+    canvas.selectAll('path')
+            .data(countyData)
+            .enter()
+            .append('path')
+            .attr('d', d3.geoPath())
+            .attr('class', 'county')
+    
+
 
 }
+
+
+d3.json(countyURL).then(
+    (data,error) => {
+        if(error){
+            console.log(log)
+        }else{
+            countyData = topojson.feature(data, data.objects.counties).features
+            
+            console.log(countyData)
+
+            d3.json(educationURL).then(
+                (data, error) => {
+                    if (error) {
+                        console.log(error)
+                    }else{
+                        educationData = data
+                        console.log(educationData)
+                        drawMap()
+                    }
+                }
+            )
+        }
+    }
+)
