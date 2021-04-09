c16d93310fa293316aaf4d33fc9f3d3d81faecf4
Ben Li <ben.li@griffithuni.edu.au>
2021-04-09 20:04:02 +1000

    finish learning of js

---
+++ Some-tests/js.playground/index.js
@@ -0,0 +1,19 @@
+for (var i=1 ; i < MimeTypeArray.length; i++){
+    
+}
+
+//transfer crv collum into on Array
+
+
+while (i < 5) {
+    myArray.push(i);
+    i++;
+}
+
+
+do {
+    myArray.push(i);
+    i++;
+} while (i < 10)
+
+// how to establish connection between html javascript?
